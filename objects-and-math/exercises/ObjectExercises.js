dd5b2f75b04d1da2657a68fd153e1e0ce7736918
Georgia Knoll <knollgeorgia@gmail.com>
2023-10-02 17:29:13 -0500

    attempt to update after reset

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -12,6 +12,28 @@ let salamander = {
    age: 5
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6
+};
+
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5
+};
+
+let microOrganism = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1
+};
+
+
 
 // After you have created the other object literals, add the astronautID property to each one.
 
